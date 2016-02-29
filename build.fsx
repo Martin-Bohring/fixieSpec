11246af8b0c2530f105ce37f7ec9b4fc10bc66c3
Martin Bohring <martin.bohring@shinternet.ch>
2016-02-29 19:32:51 +0100

    Got build running via Fake from the command line (yay)
    There is not much to build yet (ohh)

--- build.fsx
+++ build.fsx
@@ -6,10 +6,48 @@
 
 open Fake
 
-// Default Target
-Target "Default" (fun _ ->
-        trace "Nothing to see here yet"
+// --------------------------------------------------------------------------------------
+// Project metadata
+
+// The name of the project 
+// (used by attributes in AssemblyInfo, name of a NuGet package and directory in 'src')
+let project = "FixieSpec"
+
+// Short summary of the project
+// (used as description in AssemblyInfo and as a short summary for NuGet package)
+let summary = "Low friction specifications based on the fantastic Fixie test framework"
+
+// List of author names (for NuGet package)
+let authors = [ "Martin Bohring" ]
+
+// Tags for your project (for NuGet package)
+let tags = "Fixie BDD TDD testing"
+
+// File system information 
+// (<solutionFile>.sln is built during the building process)
+let solutionFile  = "FixieSpec"
+
+// Pattern specifying assemblies to be tested using Fixie
+let testAssemblies = "tests/**/bin/*Tests*.dll"
+
+let buildDir = "build"
+
+// --------------------------------------------------------------------------------------
+// Clean build results
+
+Target "Clean" (fun _ ->
+    CleanDirs [ buildDir ]
+)
+
+// --------------------------------------------------------------------------------------
+// Build library & test projects
+
+Target "Build" (fun _ ->
+    // We would like to build only one solution
+    !! (solutionFile + ".sln")
+    |> MSBuildReleaseExt  "" ["VisualStudioVersion", "12.0"] "Rebuild"
+    |> ignore
 )
 
 // start build
-RunTargetOrDefault "Default"
+RunTargetOrDefault "Build"
