7bbaf25c27e6259e5495503563090e980a0adff0
Martin Bohring <martin.bohring@shinternet.ch>
2016-03-02 21:07:35 +0100

    Started to setup unit test infrastructure using Fixie and AutoFixture by creating a AutoFixtureParameterSource and a unit test Fixie convention.

---
+++ tests/FixieSpec.Tests/AutoFixtureParameterSource.cs
@@ -0,0 +1,37 @@
+﻿namespace FixieSpecs.Tests
+{
+    using Fixie;
+    using Ploeh.AutoFixture;
+    using Ploeh.AutoFixture.AutoFakeItEasy;
+    using Ploeh.AutoFixture.Kernel;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    using Fixture = Ploeh.AutoFixture.Fixture;
+
+    /// <summary>
+    /// A <see cref="ParameterSource"/> that creates parameter values using AutoFîxture.
+    /// </summary>
+    public class AutoFixtureParameterSource : ParameterSource
+    {
+        /// <inheritdoc/>
+        public IEnumerable<object[]> GetParameters(MethodInfo method)
+        {
+            // Produces a randomly-populated object for each
+            // parameter declared on the test method, using
+            // a Fixture that has our customizations.
+
+            IFixture fixture = new Fixture().Customize(new AutoFakeItEasyCustomization());
+
+            yield return GetParameterData(method.GetParameters(), fixture);
+        }
+
+        object[] GetParameterData(ParameterInfo[] parameters, IFixture fixture)
+        {
+            return parameters
+                .Select(p => new SpecimenContext(fixture).Resolve(p.ParameterType))
+                .ToArray();
+        }
+    }
+}
---
+++ tests/FixieSpec.Tests/SelfTestConvention.cs
@@ -0,0 +1,27 @@
+﻿namespace FixieSpecs.Tests
+{
+    using System.Linq;
+
+    using Fixie;
+
+    /// <summary>
+    /// A Fixie test convention that resolves test case parameters using the <see cref="AutoFixtureParameterSource"/>.
+    /// Test fixture class names need to end with "Tests".
+    /// The test fixture class itself cannot have any constructor parameters.
+    /// </summary>
+    public class SelfTestConvention : Convention
+    {
+        public SelfTestConvention()
+        {
+            this.Classes
+                .NameEndsWith("Tests")
+                .Where(t =>
+                    t.GetConstructors()
+                    .All(ci => ci.GetParameters().Length == 0));
+
+            this.Methods.Where(mi => mi.IsPublic && mi.IsVoid());
+
+            this.Parameters.Add<AutoFixtureParameterSource>();
+        }
+    }
+}
