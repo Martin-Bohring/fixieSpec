61309c95c642c1e1ffad89796f956816478fa426
Martin Bohring <martin.bohring@shinternet.ch>
2016-03-10 08:40:34 +0100

    Added a SymbolExtensions class to allow refactoring safe access to MethodInfo reflection data
    (Taken from http://blog.functionalfun.net/search/label/LINQ)

---
+++ tests/FixieSpec.Tests/SymbolExtensions.cs
@@ -0,0 +1,68 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace FixieSpec.Tests
+{
+    using System;
+    using System.Linq.Expressions;
+    using System.Reflection;
+
+    /// <summary>
+    /// A helper mix-in class to refactor safe access <see cref="MethodInfo"/> idendified by an expression instead of a string.
+    /// </summary>
+    /// <remarks>Taken from http://blog.functionalfun.net/search/label/LINQ, but all errors are mine.</remarks>
+    public static class SymbolExtensions
+    {
+        /// <summary>
+        /// Given a lambda expression that calls a method, returns the method info.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expression">The expression.</param>
+        /// <returns></returns>
+        public static MethodInfo GetMethodInfo(Expression<Action> expression)
+        {
+            return GetMethodInfo((LambdaExpression)expression);
+        }
+
+        /// <summary>
+        /// Given a lambda expression that calls a method, returns the method info.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expression">The expression.</param>
+        /// <returns></returns>
+        public static MethodInfo GetMethodInfo<T>(Expression<Action<T>> expression)
+        {
+            return GetMethodInfo((LambdaExpression)expression);
+        }
+
+        /// <summary>
+        /// Given a lambda expression that calls a method, returns the method info.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expression">The expression.</param>
+        /// <returns></returns>
+        public static MethodInfo GetMethodInfo<T, TResult>(Expression<Func<T, TResult>> expression)
+        {
+            return GetMethodInfo((LambdaExpression)expression);
+        }
+
+        /// <summary>
+        /// Given a lambda expression that calls a method, returns the method info.
+        /// </summary>
+        /// <param name="expression">The expression.</param>
+        /// <returns></returns>
+        public static MethodInfo GetMethodInfo(LambdaExpression expression)
+        {
+            MethodCallExpression outermostExpression = expression.Body as MethodCallExpression;
+
+            if (outermostExpression == null)
+            {
+                throw new ArgumentException("Invalid Expression. Expression should consist of a Method call only.");
+            }
+
+            return outermostExpression.Method;
+        }
+    }
+}
---
+++ tests/FixieSpec.Tests/SymbolExtensionsTests.cs
@@ -0,0 +1,54 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace FixieSpec.Tests
+{
+    using System.Linq;
+
+    using Shouldly;
+
+    public class SymbolExtensionsTests
+    {
+        public void GetMethodInfo_should_return_method_info()
+        {
+            var methodInfo = SymbolExtensions.GetMethodInfo<TestClass>(c => c.AMethod());
+            methodInfo.Name.ShouldBe("AMethod");
+        }
+
+        public void GetMethodInfo_should_return_method_info_for_generic_method()
+        {
+            var methodInfo = SymbolExtensions.GetMethodInfo<TestClass>(c => c.AGenericMethod(default(int)));
+
+            methodInfo.Name.ShouldBe("AGenericMethod");
+            methodInfo.GetParameters().First().ParameterType.ShouldBe(typeof(int));
+        }
+
+        public void GetMethodInfo_should_return_method_info_for_static_method_on_static_class()
+        {
+            var methodInfo = SymbolExtensions.GetMethodInfo(() => StaticTestClass.StaticTestMethod());
+
+            methodInfo.Name.ShouldBe("StaticTestMethod");
+            methodInfo.IsStatic.ShouldBeTrue();
+        }
+
+        class TestClass
+        {
+            public void AMethod()
+            {
+            }
+
+            public void AGenericMethod<T>(T parameter)
+            {
+            }
+        }
+
+        static class StaticTestClass
+        {
+            public static void StaticTestMethod()
+            {
+            }
+        }
+    }
+}
