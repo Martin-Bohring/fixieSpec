6df8c58ae381ad1dd8dd8d58ff4b7b14d14287a5
Martin Bohring <martin.bohring@shinternet.ch>
2016-03-15 22:55:54 +0100

    Created a declaration order comparer helper class
    Added empty FixieSpecConventionTests class
    (Still thinking about how to test it)

---
+++ src/FixieSpec/DeclarationOrderComparer.cs
@@ -0,0 +1,42 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace FixieSpec
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+
+    /// <summary>
+    /// An <see cref="IComparer{T}"/> implementation that compares the declaration order of
+    /// <see cref="MemberInfo"/> elements of types.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of the <see cref="MemberInfo"/> to compare.
+    /// </typeparam>
+    /// <remarks>
+    /// There are doubts if this implementation iss using .Net framework implementation specific
+    /// properties and if it might fail in other implemenation of the CLR.
+    /// </remarks>
+    public class DeclarationOrderComparer<T> : IComparer<T>
+        where T : MemberInfo
+    {
+        /// <inheritdoc/>
+        public int Compare(T first, T second)
+        {
+            if (first.MetadataToken < second.MetadataToken)
+            {
+                return -1;
+            }
+
+            if (first.MetadataToken > second.MetadataToken)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ tests/FixieSpec.Tests/DeclarationOrderComparerTests.cs
@@ -0,0 +1,47 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace FixieSpec.Tests
+{
+    using System.Reflection;
+
+    using Shouldly;
+
+    sealed class DeclarationOrderComparerTests
+    {
+        public void ShouldCompareEarlierDeclaredMembersAsBefore()
+        {
+            var testee = new DeclarationOrderComparer<MethodInfo>();
+
+            var result = testee.Compare(
+                SymbolExtensions.GetMethodInfo<SimpleSpec>(c => c.When_executing_a_test_step()),
+                SymbolExtensions.GetMethodInfo<SimpleSpec>(c => c.And_when_executing_a_second_test_step()));
+
+            result.ShouldBe(-1);
+        }
+
+        public void ShouldCompareLaterDeclaredMembersAsAfter()
+        {
+            var testee = new DeclarationOrderComparer<MethodInfo>();
+
+            var result = testee.Compare(
+                SymbolExtensions.GetMethodInfo<SimpleSpec>(c => c.And_when_executing_a_second_test_step()),
+                SymbolExtensions.GetMethodInfo<SimpleSpec>(c => c.When_executing_a_test_step()));
+
+            result.ShouldBe(1);
+        }
+
+        public void ShouldCompareTheSameMemberAsSame()
+        {
+            var testee = new DeclarationOrderComparer<MethodInfo>();
+
+            var result = testee.Compare(
+                SymbolExtensions.GetMethodInfo<SimpleSpec>(c => c.And_when_executing_a_second_test_step()),
+                SymbolExtensions.GetMethodInfo<SimpleSpec>(c => c.And_when_executing_a_second_test_step()));
+
+            result.ShouldBe(0);
+        }
+    }
+}
