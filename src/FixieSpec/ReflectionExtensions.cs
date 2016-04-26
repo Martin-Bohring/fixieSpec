99076f92066d266ab238054989c624430e120188
Martin Bohring <martin.bohring@shinternet.ch>
2016-04-26 21:34:18 +0200

    Mini code cleanup

--- src/FixieSpec/ReflectionExtensions.cs
+++ src/FixieSpec/ReflectionExtensions.cs
@@ -8,7 +8,6 @@ namespace FixieSpec
     using System;
     using System.Linq;
     using System.Reflection;
-    using Fixie;
 
     /// <summary>
     /// An extension class that contains methods to simplify reflection based code
