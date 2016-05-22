880772244d3dfd6406c9b3e8d663585ebfd89aab
Martin Bohring <martin.bohring@shinternet.ch>
2016-05-22 16:43:35 +0200

    Fixed file headers
    Added example specification about taking phone calls

---
+++ Samples/AV.Specifications/TakingVoicePhoneCallSpecification.cs
@@ -0,0 +1,26 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace AV.Specifications
+{
+    public class TakingVoicePhoneCallSpecification
+    {
+        public void When_the_phone_rings()
+        {
+        }
+
+        public void And_when_the_callee_accepts_the_phone_call_in_headphone_mode()
+        {
+        }
+
+        public void Then_the_callee_should_hear_the_caller_on_the_headset()
+        {
+        }
+
+        public void And_then_the_callee_should_be_able_to_speak_to_the_caller_using_the_headset()
+        {
+        }
+    }
+}
