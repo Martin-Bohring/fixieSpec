7696afeb0aadd13ad9795d5c6b226a59ae56df9a
Martin Bohring <martin.bohring@shinternet.ch>
2016-05-22 18:35:39 +0200

    Started to flesh out the AV domain to support and being able to state the existing specifications.
    (Currently in its most basic form)

---
+++ Samples/AV.Domain/Devices/Audio/Headset.cs
@@ -0,0 +1,22 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace AV.Domain.Devices.Audio
+{
+    /// <summary>
+    /// A headset is a device combining a speaker and a microphone.
+    /// </summary>
+    public class Headset
+    {
+        readonly Speaker headsetSpeaker;
+        readonly Microphone headsetMicrophone;
+
+        public Headset (Microphone headsetMicrophone, Speaker headsetSpeaker)
+        {
+            this.headsetMicrophone = headsetMicrophone;
+            this.headsetSpeaker = headsetSpeaker;
+        }
+    }
+}
---
+++ Samples/AV.Domain/Devices/Audio/Speaker.cs
@@ -0,0 +1,14 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace AV.Domain.Devices.Audio
+{
+    /// <summary>
+    /// A class that represents a speaker.
+    /// </summary>
+    public abstract class Speaker
+    {
+    }
+}
