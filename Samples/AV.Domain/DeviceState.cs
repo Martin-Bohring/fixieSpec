4df6f3b109fbe791af94ec40af6a2fb48c82446f
Martin Bohring <martin.bohring@shinternet.ch>
2016-05-25 21:43:38 +0200

    Introduced a MediaDeviceBase class in the sample project
    (Still needs to be fleshed out much more)

---
+++ Samples/AV.Domain/DeviceState.cs
@@ -0,0 +1,20 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace AV.Domain
+{
+    public enum DeviceState
+    {
+        /// <summary>
+        /// The device is connected.
+        /// </summary>
+        Connected,
+
+        /// <summary>
+        /// The device has been connected once, but is not right now.
+        /// </summary>
+        Disconnected
+    }
+}
---
+++ Samples/AV.Domain/MediaDeviceBase.cs
@@ -0,0 +1,20 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace AV.Domain
+{
+    /// <summary>
+    /// A base class for media devices.
+    /// </summary>
+    public abstract class MediaDeviceBase
+    {
+        protected MediaDeviceBase(DeviceId id)
+        {
+            DeviceId = id;
+        }
+
+        public DeviceId DeviceId { get; private set; }
+    }
+}
