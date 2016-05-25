7e33a82b5d82ed7663b57bb37e28f4cbb2a4e86d
Martin Bohring <martin.bohring@shinternet.ch>
2016-05-25 21:18:28 +0200

    Created a simple device id value type

---
+++ Samples/AV.Domain.Tests/DeviceIdTests.cs
@@ -0,0 +1,53 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace AV.Domain.Tests
+{
+    using System;
+
+    using Shouldly;
+
+    class DeviceIdTests
+    {
+        public void ShouldFailWhenConstructedUsingEmptyId()
+        {
+            Action act = () => new DeviceId(Guid.Empty);
+
+            act.ShouldThrow<ArgumentException>();
+        }
+
+        public void ShouldSucceedWhenConstructedWithValidId()
+        {
+            Action act = () => new DeviceId(Guid.NewGuid());
+
+            act.ShouldNotThrow();
+        }
+
+        public void ShouldSucceedWhenConstructedWithoutId()
+        {
+            Action act = () => new DeviceId();
+
+            act.ShouldNotThrow();
+        }
+
+        public void ShouldBeEqualWithIdenticalId()
+        {
+            Guid identicalId = Guid.NewGuid();
+
+            var firstDeviceId = new DeviceId(identicalId);
+            var secondDeviceId = new DeviceId(identicalId);
+
+            firstDeviceId.Equals(secondDeviceId).ShouldBeTrue();
+        }
+
+        public void ShouldNotBeEqualWithDifferentId()
+        {
+            var firstDeviceId = new DeviceId(Guid.NewGuid());
+            var secondDeviceId = new DeviceId(Guid.NewGuid());
+
+            firstDeviceId.Equals(secondDeviceId).ShouldBeFalse();
+        }
+    }
+}
---
+++ Samples/AV.Domain/DeviceId.cs
@@ -0,0 +1,46 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace AV.Domain
+{
+    using System;
+
+    /// <summary>
+    /// Represents the unique id of a device.
+    /// </summary>
+    public class DeviceId : IEquatable<DeviceId>
+    {
+        readonly Guid deviceId;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeviceId"/> class.
+        /// </summary>
+        public DeviceId() : this(Guid.NewGuid())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeviceId"/> class.
+        /// </summary>
+        /// <param name="id">
+        /// The id of the device.
+        /// </param>
+        public DeviceId(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                throw new ArgumentException("Invalid device id", nameof(id));
+            }
+
+            deviceId = id;
+        }
+
+        /// <inheritdoc/>
+        public bool Equals(DeviceId other)
+        {
+            return deviceId == other.deviceId;
+        }
+    }
+}
