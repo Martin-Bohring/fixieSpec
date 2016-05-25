a0970092152f3067a82520479d1c5bab1ab04bbc
Martin Bohring <martin.bohring@shinternet.ch>
2016-05-25 23:24:19 +0200

    Reboot in sample domain: Start with the most simple use case and let further tests drive the sample domain

---
+++ Samples/Media.Domain/Microphone.cs
@@ -0,0 +1,30 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace Media.Domain
+{
+    /// <summary>
+    /// Represents a microphone.
+    /// </summary>
+    public class Microphone : DeviceBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Microphone"/> class.
+        /// </summary>
+        public Microphone() : base(new DeviceId())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Microphone"/> class.
+        /// </summary>
+        /// <param name="id">
+        /// The id of the microphone.
+        /// </param>
+        public Microphone(DeviceId id) : base(id)
+        {
+        }
+    }
+}
---
+++ Samples/Media.Specifications/RecordVoiceMemoSpecifications.cs
@@ -0,0 +1,37 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace Media.Specifications
+{
+    using Domain;
+
+    using FixieSpec;
+
+    [Inconclusive]
+    public class VoiceMemoRecorSpecifications
+    {
+        Microphone microphone = new Microphone();
+
+        public void Given_a_microphone_is_available()
+        {
+        }
+
+        public void When_the_microphone_is_selected_for_voice_memo_recording()
+        {
+        }
+
+        public void And_when_the_voice_memo_recording_is_started()
+        {
+        }
+
+        public void Then_the_selected_microphone_is_used_for_recording()
+        {
+        }
+
+        public void And_then_the_microphone_is_not_available_anymore()
+        {
+        }
+    }
+}
