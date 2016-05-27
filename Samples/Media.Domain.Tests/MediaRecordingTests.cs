f62739673b3cb336f170af020b26d6b3daa11c1e
Martin Bohring <martin.bohring@shinternet.ch>
2016-05-27 20:49:31 +0200

    Introduced the concept of a media recording into the domain

---
+++ Samples/Media.Domain.Tests/MediaRecordingTests.cs
@@ -0,0 +1,21 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace Media.Domain.Tests
+{
+    using System;
+
+    using Shouldly;
+
+    public class MediaRecordingTests
+    {
+        public void ShouldFailWhenConstructedUsingNullDevice()
+        {
+            Action act = () => new MediaRecording(null);
+
+            act.ShouldThrow<ArgumentNullException>();
+        }
+    }
+}
---
+++ Samples/Media.Domain/MediaRecording.cs
@@ -0,0 +1,33 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace Media.Domain
+{
+    using System;
+
+    /// <summary>
+    /// Represents an ongoing media recording.
+    /// </summary>
+    public class MediaRecording
+    {
+        readonly DeviceBase mediaSource;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MediaRecording"/> class.
+        /// </summary>
+        /// <param name="mediaSource">
+        /// The device providing the media to be recorded.
+        /// </param>
+        public MediaRecording(DeviceBase mediaSource)
+        {
+            if (mediaSource == null)
+            {
+                throw new ArgumentNullException(nameof(mediaSource));
+            }
+
+            this.mediaSource = mediaSource;
+        }
+    }
+}
