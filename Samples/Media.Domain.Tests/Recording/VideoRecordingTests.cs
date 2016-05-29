11db00be394633a76ce07a3fbd8c2b38d6a526cc
Martin Bohring <martin.bohring@shinternet.ch>
2016-05-30 00:02:05 +0200

    Sample domain: Introduced a video recording class

---
+++ Samples/Media.Domain.Tests/Recording/VideoRecordingTests.cs
@@ -0,0 +1,23 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+
+namespace Media.Domain.Tests.Recording
+{
+    using System;
+
+    using Shouldly;
+    using Domain.Recording;
+
+    public class VideoRecordingTests
+    {
+        public void ShouldFailWhenConstructedUsingNullVideoSourceDevice()
+        {
+            Action act = () => new VideoRecording(null);
+
+            act.ShouldThrow<ArgumentNullException>();
+        }
+    }
+}
---
+++ Samples/Media.Domain/Recording/VideoRecording.cs
@@ -0,0 +1,33 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace Media.Domain.Recording
+{
+    using System;
+
+    /// <summary>
+    /// Represents an ongoing video recording.
+    /// </summary>
+    public class VideoRecording
+    {
+        readonly DeviceBase videoSource;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VideoRecording"/> class.
+        /// </summary>
+        /// <param name="videoSource">
+        /// The device providing the video signal to be recorded.
+        /// </param>
+        public VideoRecording(DeviceBase videoSource)
+        {
+            if (videoSource == null)
+            {
+                throw new ArgumentNullException(nameof(videoSource));
+            }
+
+            this.videoSource = videoSource;
+        }
+    }
+}
