de6f30614fd55b3f631bea820a67030c55045df5
Martin Bohring <martin.bohring@shinternet.ch>
2016-05-30 00:07:42 +0200

    Sample domain: name space cleanup

--- Samples/Media.Domain.Tests/Recording/VideoRecordingTests.cs
+++ Samples/Media.Domain.Tests/Recording/VideoRecordingTests.cs
@@ -3,14 +3,12 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 // </copyright>
 
-
-namespace Media.Domain.Tests.Recording
+namespace Media.Domain.Recording.Tests
 {
     using System;
 
     using Shouldly;
-    using Domain.Recording;
-
+ 
     public class VideoRecordingTests
     {
         public void ShouldFailWhenConstructedUsingNullVideoSourceDevice()
