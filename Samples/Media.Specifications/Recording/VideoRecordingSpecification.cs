030b8a074c4aee9e4e836d8f272bcb3a16abf3e6
Martin Bohring <martin.bohring@shinternet.ch>
2016-05-29 21:29:57 +0200

    Sample domain: Created an yet inconclusive video recording specification

---
+++ Samples/Media.Specifications/Recording/VideoRecordingSpecification.cs
@@ -0,0 +1,50 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace Media.Recording.Specification
+{
+    using FixieSpec;
+
+    [Inconclusive]
+    public class VideoRecordingSpecification
+    {
+        public void Given_a_camera_is_available()
+        {
+        }
+
+        public void And_given_a_microphone_is_available()
+        {
+        }
+
+        public void When_the_video_recording_is_started()
+        {
+        }
+
+        [Inconclusive]
+        public void Then_the_video_recording_should_be_recording()
+        {
+        }
+
+        [Inconclusive]
+        public void Then_the_selected_camera_is_used_for_recording()
+        {
+        }
+
+        [Inconclusive]
+        public void And_then_the_selected_microphone_is_used_for_recording()
+        {
+        }
+
+        [Inconclusive]
+        public void And_then_the_selected_camera_is_not_available_anymore()
+        {
+        }
+
+        [Inconclusive]
+        public void And_then_the_selected_microphone_is_not_available_anymore()
+        {
+        }
+    }
+}
