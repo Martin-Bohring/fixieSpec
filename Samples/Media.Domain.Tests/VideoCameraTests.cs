738c16f757868530b732f9d44bb3e0cbdb896286
Martin Bohring <martin.bohring@shinternet.ch>
2016-05-30 18:19:21 +0200

    Sample domain: Created a simple video camera entity

---
+++ Samples/Media.Domain.Tests/VideoCameraTests.cs
@@ -0,0 +1,44 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace Media.Domain.Tests
+{
+    using Shouldly;
+
+    public sealed class VideoCameraTests : DeviceBaseTests<VideoCamera>
+    {
+        public void ShouldGenerateDeviceIdWhenConstructedWithoutDeviceId()
+        {
+            var videoCamera = new VideoCamera();
+
+            videoCamera.DeviceId.ShouldNotBeNull();
+        }
+
+        [Input(DeviceRole.Idle, DeviceRole.Recording, true)]
+        [Input(DeviceRole.Background, DeviceRole.Recording, false)]
+        [Input(DeviceRole.Playback, DeviceRole.Recording, false)]
+        [Input(DeviceRole.Recording, DeviceRole.Recording, false)]
+        [Input(DeviceRole.Communication, DeviceRole.Recording, false)]
+        [Input(DeviceRole.Prompt, DeviceRole.Recording, false)]
+        [Input(DeviceRole.Alert, DeviceRole.Recording, false)]
+        public void ShouldOnlyAssumeRoleWhenAvailable(
+            DeviceRole previousRole,
+            DeviceRole roleToAssume,
+            bool shouldAssumeRole)
+        {
+            var videoCamera = CreateDevice(new DeviceId());
+            videoCamera.SelectFor(previousRole);
+
+            var hasAssumedRole = videoCamera.SelectFor(roleToAssume);
+
+            hasAssumedRole.ShouldBe(shouldAssumeRole);
+        }
+
+        protected override VideoCamera CreateDevice(DeviceId id)
+        {
+            return new VideoCamera(id);
+        }
+    }
+}
---
+++ Samples/Media.Domain/VideoCamera.cs
@@ -0,0 +1,43 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace Media.Domain
+{
+    /// <summary>
+    /// Represents a video camera.
+    /// </summary>
+    public class VideoCamera : DeviceBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VideoCamera"/> class.
+        /// </summary>
+        public VideoCamera()
+            : base(new DeviceId())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VideoCamera"/> class.
+        /// </summary>
+        /// <param name="id">
+        /// The id of the video camera device.
+        /// </param>
+        public VideoCamera(DeviceId id)
+            : base(id)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override bool SelectFor(DeviceRole roleToAssume)
+        {
+            if (IsAvailable())
+            {
+                return base.SelectFor(roleToAssume);
+            }
+
+            return false;
+        }
+    }
+}
