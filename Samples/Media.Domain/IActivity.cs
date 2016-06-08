eb017792a09d2deef333d53d90e9bdf2dab7fd73
Martin Bohring <martin.bohring@shinternet.ch>
2016-06-08 22:48:28 +0200

    Introduced an interface to identify ongoing activities in the sample domain

---
+++ Samples/Media.Domain/IActivity.cs
@@ -0,0 +1,18 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace Media.Domain
+{
+    /// <summary>
+    /// An interface that allows to identify an ongoing activity.
+    /// </summary>
+    public interface IActivity
+    {
+        /// <summary>
+        /// Gets the <see cref="ActivityId"/> of the activity.
+        /// </summary>
+        ActivityId ActivityId { get; }
+    }
+}
