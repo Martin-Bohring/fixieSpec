181b44016f9da2e5a81c187f1914e17c80540ef5
Martin Bohring <martin.bohring@shinternet.ch>
2016-06-08 22:39:17 +0200

    Introduced an ActivityId in the sample domain to enable tracking device participation in media recording and other activities

---
+++ Samples/Media.Domain.Tests/ActivityIdTests.cs
@@ -0,0 +1,62 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace Media.Domain.Tests
+{
+    using System;
+
+    using Shouldly;
+
+    public sealed class ActivityIdTests
+    {
+        public void ShouldFailWhenConstructedUsingEmptyId()
+        {
+            Action act = () => new ActivityId(Guid.Empty);
+
+            act.ShouldThrow<ArgumentException>();
+        }
+
+        public void ShouldSucceedWhenConstructedWithValidId()
+        {
+            Action act = () => new ActivityId(Guid.NewGuid());
+
+            act.ShouldNotThrow();
+        }
+
+        public void ShouldSucceedWhenConstructedWithoutId()
+        {
+            Action act = () => new ActivityId();
+
+            act.ShouldNotThrow();
+        }
+
+        public void ShouldBeEqualWithIdenticalId()
+        {
+            Guid identicalId = Guid.NewGuid();
+
+            var firstActivityId = new ActivityId(identicalId);
+            var secondActivityId = new ActivityId(identicalId);
+
+            firstActivityId.Equals(secondActivityId).ShouldBeTrue();
+        }
+
+        public void ShouldNotBeEqualWithDifferentId()
+        {
+            var firstActivityId = new ActivityId(Guid.NewGuid());
+            var secondActivityId = new ActivityId(Guid.NewGuid());
+
+            firstActivityId.Equals(secondActivityId).ShouldBeFalse();
+        }
+
+        public void ShouldBeEqualWithSameInstance()
+        {
+            var activityId = new ActivityId(Guid.NewGuid());
+
+#pragma warning disable RECS0088 // Comparing equal expression for equality is usually useless
+            activityId.Equals(activityId).ShouldBeTrue();
+#pragma warning restore RECS0088 // Comparing equal expression for equality is usually useless
+        }
+    }
+}
---
+++ Samples/Media.Domain/ActivityId.cs
@@ -0,0 +1,47 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace Media.Domain
+{
+    using System;
+
+    /// <summary>
+    /// Represents the unique id of an activity.
+    /// </summary>
+    public class ActivityId : IEquatable<ActivityId>
+    {
+        readonly Guid activityId;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ActivityId"/> class.
+        /// </summary>
+        public ActivityId()
+            : this(Guid.NewGuid())
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ActivityId"/> class.
+        /// </summary>
+        /// <param name="id">
+        /// The id of the activity.
+        /// </param>
+        public ActivityId(Guid id)
+        {
+            if (id == Guid.Empty)
+            {
+                throw new ArgumentException("Invalid device id", nameof(id));
+            }
+
+            activityId = id;
+        }
+
+        /// <inheritdoc/>
+        public bool Equals(ActivityId other)
+        {
+            return activityId == other.activityId;
+        }
+    }
+}
