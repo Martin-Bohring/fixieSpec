8380168cd4b8eb1b7f735ea11960f98d4861a954
Martin Bohring <martin.bohring@shinternet.ch>
2016-06-16 22:41:03 +0200

    Introduced the concept of RoleInActivity within the sample domain
    I have run into an issue with AutoFixture.Idioms when having static null values within types.
    I have raised an issue with them (https://github.com/AutoFixture/AutoFixture/issues/656)

---
+++ Samples/Media.Domain/RoleInActivity.cs
@@ -0,0 +1,53 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace Media.Domain
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Represents a role within an activity.
+    /// </summary>
+    public class RoleInActivity : ValueObject<RoleInActivity>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RoleInActivity"/> class.
+        /// </summary>
+        /// <param name="role">
+        /// The role within the activity.
+        /// </param>
+        /// <param name="activity">
+        /// The activity a role.
+        /// </param>
+        public RoleInActivity(DeviceRole role, ActivityId activity)
+        {
+            if (activity == null)
+            {
+                throw new ArgumentNullException(nameof(activity));
+            }
+
+            Role = role;
+            Activity = activity;
+        }
+
+        /// <summary>
+        /// Gets the activity a role is being played in.
+        /// </summary>
+        public ActivityId Activity { get; private set; }
+
+        /// <summary>
+        /// Gets the role within the activity.
+        /// </summary>
+        public DeviceRole Role { get; private set; }
+
+        /// <inheritdoc/>
+        protected override IEnumerable<object> Reflect()
+        {
+            yield return Role;
+            yield return Activity;
+        }
+    }
+}
