a797c90d99f3c919eaf305be861217b0825979f0
Martin Bohring <martin.bohring@shinternet.ch>
2016-08-08 01:46:39 +0200

    Improved SuccessfulSpecificationExecution to also pass instance values from setup steps to transition steps to assertion steps
    via member variables to prove the same specification instance is used for all steps

--- tests/FixieSpec.Specifications/SuccessfulSPecificationExecution.cs
+++ tests/FixieSpec.Specifications/SuccessfulSPecificationExecution.cs
@@ -44,8 +44,14 @@ namespace FixieSpec.Specifications
 
         class SuccessfulSpecification
         {
+            const int SomeValue = 42;
+            const int SomeOtherValue = 4711;
+
+            SpecificationContext systemUnderTest;
+
             public void Given_a_specification_context()
             {
+                systemUnderTest = new SpecificationContext();
                 WhereAmI();
             }
 
@@ -56,22 +62,32 @@ namespace FixieSpec.Specifications
 
             public void When_exercising_the_system_under_test()
             {
+                systemUnderTest.Value = SomeValue;
                 WhereAmI();
             }
 
             public void And_when_exercising_the_system_under_test_some_more()
             {
+                systemUnderTest.AnotherValue = SomeOtherValue;
                 WhereAmI();
             }
 
             public void Then_a_result_can_be_verified()
             {
+                systemUnderTest.Value.ShouldBe(SomeValue);
                 WhereAmI();
             }
 
             public void And_then_another_result_can_be_verified()
             {
+                systemUnderTest.AnotherValue.ShouldBe(SomeOtherValue);
                 WhereAmI();
+            }
+
+            class SpecificationContext
+            {
+                public int Value { get; set; }
+                public int AnotherValue { get; set; }
             }
         }
     }
