f34f2b854310a635f64130da74eca442a888a9e5
Martin Bohring <martin.bohring@shinternet.ch>
2016-08-08 00:30:21 +0200

    Added self test convention to pick up specifications, but not example specifications by using a marker base class
    (We will see how this will work out)

---
+++ tests/FixieSpec.Specifications/FixieSpecSpecificationBase.cs
@@ -0,0 +1,11 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace FixieSpec.Specifications
+{
+    public abstract class FixieSpecSpecificationBase
+    {
+    }
+}
---
+++ tests/FixieSpec.Specifications/SelfTestSpecificationConvention.cs
@@ -0,0 +1,19 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace FixieSpec.Specifications
+{
+    using FixieSpec;
+
+    public class SelfTestSpecificationConvention : FixieSpecConvention
+    {
+        public SelfTestSpecificationConvention()
+        {
+            Classes
+                .Where(type => type.HasOnlyDefaultConstructor())
+                .Where(type => type.IsSubclassOf(typeof(FixieSpecSpecificationBase)));
+        }
+    }
+}
