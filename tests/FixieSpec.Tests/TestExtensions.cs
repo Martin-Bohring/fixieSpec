255c122404a4db34a38c286b42d7f704a4db3796
Martin Bohring <martin.bohring@shinternet.ch>
2016-08-08 22:24:31 +0200

    Removed obsoleted unit tests completely and also supporting test infrastructure code when no more used

--- tests/FixieSpec.Tests/TestExtensions.cs
+++ tests/FixieSpec.Tests/TestExtensions.cs
@@ -6,14 +6,7 @@
 namespace FixieSpec.Tests
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
     using System.Reflection;
-
-    using Fixie;
-    using Fixie.Execution;
-    using Fixie.Internal;
-    using Shouldly;
 
     /// <summary>
     /// Test helper extension class taken from
@@ -26,33 +19,6 @@ namespace FixieSpec.Tests
         public static MethodInfo GetInstanceMethod(this Type type, string methodName)
         {
             return type.GetMethod(methodName, InstanceMethods);
-        }
-
-        public static AssemblyResult Run(this Type sampleTestClass, Listener listener, Convention convention)
-        {
-            return new Runner(listener).RunTypes(sampleTestClass.Assembly, convention, sampleTestClass);
-        }
-
-        public static IEnumerable<string> Lines(this RedirectedConsole console)
-        {
-            return console.Output.Lines();
-        }
-
-        public static IEnumerable<string> Lines(this string multiline)
-        {
-            var lines = multiline.Split(new[] { Environment.NewLine }, StringSplitOptions.None).ToList();
-
-            while (lines.Count > 0 && lines[lines.Count - 1] == string.Empty)
-            {
-                lines.RemoveAt(lines.Count - 1);
-            }
-
-            return lines;
-        }
-
-        public static void ShouldEqual<T>(this IEnumerable<T> actual, params T[] expected)
-        {
-            actual.ToArray().ShouldBe(expected);
         }
     }
 }
