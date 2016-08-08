e5fbd63e5161dc163a7d6de6a2ebf70c44551381
Martin Bohring <martin.bohring@shinternet.ch>
2016-08-08 18:47:52 +0200

    Further role name changes in specifications triggered by the recent specification name changes

--- tests/FixieSpec.Specifications/FailedAssertionStepExecution.cs
+++ tests/FixieSpec.Specifications/FailedAssertionStepExecution.cs
@@ -10,16 +10,16 @@ namespace FixieSpec.Specifications
 
     public sealed class FailedAssertionStepExecution : FixieSpecSpecificationBase
     {
-        SpecificationExecutionResult failedExecutionResult;
+        SpecificationExecutionResult failedAssertionStepExecutionResult;
 
         public void When_executing_an_assertion_step_fails()
         {
-            failedExecutionResult = Execute<FailingAssertionStepSpecification>();
+            failedAssertionStepExecutionResult = Execute<FailingAssertionStepSpecification>();
         }
 
         public void Then_all_specifiction_steps_should_execute_in_order()
         {
-            failedExecutionResult.ConsoleOutput.ShouldEqual(
+            failedAssertionStepExecutionResult.ConsoleOutput.ShouldEqual(
                 "When_exercising_the_system_under_test",
                 "And_when_exercising_the_system_under_test_some_more",
                 "Then_a_failing_result_can_be_verified",
@@ -28,17 +28,17 @@ namespace FixieSpec.Specifications
 
         public void And_then_all_specification_steps_should_be_recognized()
         {
-            failedExecutionResult.Total.ShouldBe(2);
+            failedAssertionStepExecutionResult.Total.ShouldBe(2);
         }
 
         public void And_then_all_successful_assertion_steps_should_be_recognized()
         {
-            failedExecutionResult.Passed.ShouldBe(1);
+            failedAssertionStepExecutionResult.Passed.ShouldBe(1);
         }
 
         public void And_then_all_failed_assertion_steps_should_be_recognized()
         {
-            failedExecutionResult.Failed.ShouldBe(1);
+            failedAssertionStepExecutionResult.Failed.ShouldBe(1);
         }
 
         class FailingAssertionStepSpecification
