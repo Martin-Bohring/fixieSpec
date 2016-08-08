244ab9dbdeaa28b73db87499e95d12ebab82b61d
Martin Bohring <martin.bohring@shinternet.ch>
2016-08-08 17:59:03 +0200

    Added specification of executing a specification having a failed setup step
    (Again I had to duplicate some test support code, but this will be cleaned up before merging)

---
+++ tests/FixieSpec.Specifications/ShouldBeUnreachableException.cs
@@ -0,0 +1,22 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace FixieSpec.Specifications
+{
+    using System;
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// Took the idea from
+    /// https://github.com/fixie/fixie/blob/master/src/Fixie.Tests/ShouldBeUnreachableException.cs.
+    /// </summary>
+    [Serializable]
+    public class ShouldBeUnreachableException : Exception
+    {
+        public ShouldBeUnreachableException([CallerMemberName] string member = null)
+            : base($"'{member}' reached a line of code thought to be unreachable.")
+        {}
+    }
+}
