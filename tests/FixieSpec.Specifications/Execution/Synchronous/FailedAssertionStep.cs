3697392bafffff3a9cbed43a498fa5be6c0dff9d
Martin Bohring <martin.bohring@shinternet.ch>
2016-08-15 19:33:26 +0200

    Fixed code formatting problem

--- tests/FixieSpec.Specifications/Execution/Synchronous/FailedAssertionStep.cs
+++ tests/FixieSpec.Specifications/Execution/Synchronous/FailedAssertionStep.cs
@@ -3,7 +3,6 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 // </copyright>
 
-
 namespace FixieSpec.Specifications.Execution.Synchronous
 {
     using Shouldly;
