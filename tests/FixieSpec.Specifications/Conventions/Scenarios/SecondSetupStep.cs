387bcc98a0a4df6b25dc55fd989ba01a2b799693
Martin Bohring <martin.bohring@shinternet.ch>
2016-09-09 15:51:54 +0200

    First preparation step to create specifications covering the conventions
    (I am moving towards specifications for high level tests, and keep only code mechanics level tests like parameter guarding as unit tests )

---
+++ tests/FixieSpec.Specifications/Conventions/Scenarios/SecondSetupStep.cs
@@ -0,0 +1,50 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace FixieSpec.Specifications.Conventions.Scenarios
+{
+    [Inconclusive]
+    public sealed class SecondSetupStep
+    {
+        public void When_a_specification_has_two_setup_steps()
+        {
+        }
+
+        public void Then_it_should_not_be_a_valid_specification()
+        {
+        }
+
+        public void And_then_attempting_to_execute_it_should_fail()
+        {
+        }
+
+        public void And_then_all_assertion_steps_should_fail()
+        {
+        }
+
+        public void And_then_there_should_be_no_successful_assertion_steps()
+        {
+        }
+
+        class SecondSetupStepSpecification
+        {
+            public void Given_a_setup_step()
+            {
+            }
+
+            public void Given_a_second_setup_step()
+            {
+            }
+
+            public void Then_the_result_cannot_be_verified()
+            {
+            }
+
+            public void And_then_another_result_can_also_not_be_verified()
+            {
+            }
+        }
+    }
+}
