5aa5131b9536baf2d1868113468647d0dae7a0f5
Martin Bohring <martin.bohring@shinternet.ch>
2016-09-09 16:09:28 +0200

    Created an empty specification for setup step detection

---
+++ tests/FixieSpec.Specifications/Conventions/Steps/SetupSteps.cs
@@ -0,0 +1,28 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace FixieSpec.Specifications.Conventions.Steps
+{
+    [Inconclusive]
+    public sealed class SetupSteps
+    {
+        public void When_detecting_setup_steps()
+        {
+        }
+
+        public void Then_setup_steps_are_detected()
+        {
+        }
+
+        public void And_then_additional_setup_steps_are_detected()
+        {
+        }
+
+        public void And_then_other_methods_are_not_detected_as_setup_steps()
+        {
+
+        }
+    }
+}
