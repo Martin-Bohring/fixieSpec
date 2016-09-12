7d4a65927f50e40fccf9cc1ee24643c64e1c3e7a
Martin Bohring <martin.bohring@shinternet.ch>
2016-09-12 12:56:30 +0200

    Migrated testing transition step conventions from unit test to specifications
    (Observation: More code involved, but the new code itself is simpler)

---
+++ tests/FixieSpec.Specifications/Conventions/Steps/TransitionSteps.cs
@@ -0,0 +1,83 @@
+﻿// <copyright>
+// Copyright (c) Martin Bohring. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+// </copyright>
+
+namespace FixieSpec.Specifications.Conventions.Steps
+{
+    using System.Reflection;
+    using System.Threading.Tasks;
+
+    using Shouldly;
+
+    public sealed class TransitionSteps
+    {
+        MethodInfo transitionStep;
+        MethodInfo additionalTransitionStep;
+        MethodInfo otherMethod;
+        MethodInfo methodWithParameter;
+        MethodInfo methodWithReturnValue;
+
+        public void When_detecting_transition_steps()
+        {
+            transitionStep = Method("When_exercising_the_system_under_test");
+            additionalTransitionStep = Method("And_when_exercising_the_system_under_test_some_more");
+            otherMethod = Method("ToString");
+            methodWithParameter = Method("And_when_a_non_transition_step_has_parameters");
+            methodWithReturnValue = Method("And_when_a_non_transition_step_returns_a_value");
+        }
+
+        public void Then_transition_steps_are_detected_as_transition_steps()
+        {
+            transitionStep.IsTransitionStep().ShouldBeTrue();
+        }
+
+        public void And_then_additional_transition_steps_are_detected_as_additional_transition_steps()
+        {
+            additionalTransitionStep.IsTransitionStep().ShouldBeTrue();
+        }
+
+        public void And_then_other_methods_are_not_detected_as_transition_steps()
+        {
+            otherMethod.IsTransitionStep().ShouldBeFalse();
+        }
+
+        public void And_then_methods_with_parameters_are_not_detected_as_transition_steps()
+        {
+            methodWithParameter.IsTransitionStep().ShouldBeFalse();
+        }
+
+        public void And_then_methods_with_return_values_are_not_detected_as_transition_steps()
+        {
+            methodWithReturnValue.IsTransitionStep().ShouldBeFalse();
+        }
+
+        static MethodInfo Method(string methodName)
+        {
+            return typeof(SpecificationWithTransitionSteps).GetInstanceMethod(methodName);
+        }
+
+        sealed class SpecificationWithTransitionSteps
+        {
+            public void When_exercising_the_system_under_test()
+            {
+            }
+
+            public async Task And_when_exercising_the_system_under_test_some_more()
+            {
+                await Task.FromResult(true);
+            }
+
+            public void And_when_a_non_transition_step_has_parameters(int parameter)
+            {
+                var notUsed = parameter;
+
+            }
+
+            public int And_when_a_non_transition_step_returns_a_value()
+            {
+                return 0;
+            }
+        }
+    }
+}
